febda1fc08cb6cea83aa8aa741a225cbbb5686d3
MXNT4NN4. <rique.souza06@icloud.com>
2025-02-12 10:28:17 -0300

    Create Code2.3.c

---
+++ Aula02/Code2.3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+  printf("Escreva um programa que realiza este cálculo e exibe o valor correto com a mensagem na tela: Vamos distribuir 359 carros para 4 pessoas e portanto devem sobrar %d carros.",359%4)
+return 0;
+}
