9a743427b63ce3d5ffbdde8f706117c331766845
MXNT4NN4. <rique.souza06@icloud.com>
2025-02-12 10:32:32 -0300

    Create 2.4Code.c

---
+++ Aula02/2.4Code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(void){
+ printf("Exiba as seguintes mensagens na tela, fazendo os cálculos necessários pelo programa: Eu tenho um círculo de raio 5. Portanto o diâmetro é %d A área é %f E a medida da circunferência é %f", 5*2, 3.14*(5*5), (2*3)*14*5);
+
+return 0;
+}
