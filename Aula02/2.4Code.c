af9f9da83ab281e52421c1d737162d09688f6e83
MXNT4NN4. <rique.souza06@icloud.com>
2025-02-16 20:47:58 -0300

    Update 2.4Code.c

--- Aula02/2.4Code.c
+++ Aula02/2.4Code.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-  printf("Escreva um programa que realiza este cálculo e exibe o valor correto com a mensagem na tela: Vamos distribuir 359 carros para 4 pessoas e portanto devem sobrar %d carros.",359%4)
+  printf("Escreva um programa que realiza este cálculo e exibe o valor correto com a mensagem na tela: Vamos distribuir 359 carros para 4 pessoas e portanto devem sobrar %d carros.",359%4);
 return 0;
 }
