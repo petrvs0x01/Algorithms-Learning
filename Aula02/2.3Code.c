fdebbdc8ce6563193efd0152c6faa0f2d0767ec1
MXNT4NN4. <rique.souza06@icloud.com>
2025-02-16 21:58:33 -0300

    Update 2.3Code.c

--- Aula02/2.3Code.c
+++ Aula02/2.3Code.c
@@ -6,3 +6,5 @@ int main(void){
 
 return 0;
 }
+
+// 3 -  Escreva um programa que realiza este cálculo e exibe o valor correto com a mensagem na tela: Vamos distribuir 359 carros para 4 pessoas e portanto devem sobrar ___ carros.
