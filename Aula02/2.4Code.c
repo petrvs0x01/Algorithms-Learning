1d61817da9c456be3cfa274e315f937f9d305fbb
MXNT4NN4. <rique.souza06@icloud.com>
2025-02-18 23:28:42 -0300

    Delete Aula02 directory

--- Aula02/2.4Code.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-  printf("Escreva um programa que realiza este cálculo e exibe o valor correto com a mensagem na tela: Vamos distribuir 359 carros para 4 pessoas e portanto devem sobrar %d carros.",359%4);
-return 0;
-}
-
-// Exiba as seguintes mensagens na tela, fazendo os cálculos necessários pelo programa: Eu tenho um círculo de raio 5. Portanto o diâmetro é ___ A área é ___ E a medida da circunferência é ___
