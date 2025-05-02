9ca565db2e07744b9d5bb36244546ad9df7e033f
Petrvs <rique.souza06@icloud.com>
2025-05-02 00:10:29 -0300

    Create Aula11.5.c

---
+++ Aulas-BCC/Aula11/Aula11.5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+    /* Escreva uma função chamada Potencia(base, expoente) que, quando chamada, retorna baseexpoente. Por exemplo, potencia(2, 5) deve retornar 32. 
+    Assuma que expoente é sempre um número inteiro maior ou igual a 1. 
+    Sua função deve calcular o valor através de uma estrutura de repetição 
+    sem chamar a função de potência da linguagem C.*/ 
+
+int Mypow(int base,int exponent){
+    
+    int i = 0,result = 1;
+    while(i < exponent){
+        result *= base;
+        i++;
+    }
+     return result;
+    
+}
+
+int main()
+{
+    int base, exponent;
+    printf("Entre dois número: Base - Expoente");
+    scanf("%d",&base);
+    scanf("%d",&exponent);
+    printf("%d",Mypow(base,exponent));
+    
+    return 0;
+}
