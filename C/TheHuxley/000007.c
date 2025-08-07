c6e5a7848aeb4d89d6f41d2c2c45509d03324ad6
Petrvs Henricvs <rique.souza06@icloud.com>
2025-08-07 17:47:36 -0300

    Update 000007.c

--- C/TheHuxley/000007.c
+++ C/TheHuxley/000007.c
@@ -1,3 +1,18 @@
+/*
+Ambrósio é amigo da vizinhança e resolveu 
+dar descontos para agradar seus clientes. 
+A mercearia de Ambrósio contém apenas 04 itens
+cujos preços são dados pela tabela abaixo:
+A regra de desconto é bem simples: se a 
+quantidade de produtos comprados for igual ou maior que quinze, então o desconto é concedido. O desconto também é concedido 
+caso o valor total da compra seja maior ou igual a 40 reais.
+O valor do desconto é de 15%.
+Sua missão é fazer um programa que leia o código do produto, a quantidade comprada e imprima o valor que o cliente deve pagar
+já considerando o desconto quando aplicável.
+Considere que o cliente só pode comprar um único 
+tipo produto cada vez que usar o seu software.
+*/
+
 #include <stdio.h>
 
 int main() {
